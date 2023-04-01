b124415c4ac2fd034245d18d66e12040816aa7e9
gegehprast <gegeh.prast@gmail.com>
2023-04-02 01:30:11 +0700

    middlewares and response types

---
+++ middlewares/withCORS.ts
@@ -0,0 +1,34 @@
+import { ApiMiddleware } from '@/Type'
+
+const withCORS: ApiMiddleware = handler => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+
+    if (req.headers.origin === 'http://localhost:3000') {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+    } else {
+        res.setHeader(
+            'Access-Control-Allow-Origin',
+            process.env.ALLOWED_ORIGIN as string
+        )
+    }
+
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+    )
+
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+
+        return
+    }
+
+    return await handler(req, res)
+}
+
+export default withCORS
