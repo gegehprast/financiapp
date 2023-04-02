6cc8a731c793f062f88a65abc738315da279903e
gegehprast <gegeh.prast@gmail.com>
2023-04-03 04:56:01 +0700

    update layouting

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,8 +15,8 @@ export default function Home() {
             </section>
 
             {/* wallets */}
-            <section className="flex flex-col p-3 mt-5 bg-white rounded-lg">
-                <div className="flex flex-row items-center justify-between">
+            <section className="flex flex-col mt-5 bg-white rounded-lg">
+                <div className="flex flex-row items-center justify-between p-3">
                     <h2 className="text-lg font-semibold">My Wallets</h2>
                     <Link href={'/wallets'} className="font-medium text-green-400">
                         See all
@@ -24,44 +24,62 @@ export default function Home() {
                 </div>
 
                 <ul>
-                    <li className="flex flex-row items-center justify-between pt-2 mt-2 border-t">
-                        <div className="flex flex-row items-center">
-                            <div className="w-6 h-6">
-                                <CreditCard />
-                            </div>
-                            <div className="ml-3 font-medium">Rekening BCA</div>
-                        </div>
+                    <li className="group hover:bg-gray-400">
+                        <Link href={'/wallet'} className="flex flex-col px-3 pb-3">
+                            <div className="w-full border-t group-hover:border-gray-400"></div>
 
-                        <div className="font-medium">Rp6,787,844</div>
+                            <div className="flex flex-row items-center justify-between mt-3">
+                                <div className="flex flex-row items-center">
+                                    <div className="w-6 h-6">
+                                        <CreditCard />
+                                    </div>
+                                    <div className="ml-3 font-medium">Rekening BCA</div>
+                                </div>
+
+                                <div className="font-medium">Rp6,787,844</div>
+                            </div>
+                        </Link>
                     </li>
 
-                    <li className="flex flex-row items-center justify-between pt-2 mt-2 border-t">
-                        <div className="flex flex-row items-center">
-                            <div className="w-6 h-6">
-                                <CreditCard />
-                            </div>
-                            <div className="ml-3 font-medium">Jenius</div>
-                        </div>
+                    <li className="group hover:bg-gray-400">
+                        <Link href={'/wallet'} className="flex flex-col px-3 pb-3">
+                            <div className="w-full border-t group-hover:border-gray-400"></div>
 
-                        <div className="font-medium">Rp543,844</div>
+                            <div className="flex flex-row items-center justify-between mt-3">
+                                <div className="flex flex-row items-center">
+                                    <div className="w-6 h-6">
+                                        <CreditCard />
+                                    </div>
+                                    <div className="ml-3 font-medium">Jenius</div>
+                                </div>
+
+                                <div className="font-medium">Rp543,844</div>
+                            </div>
+                        </Link>
                     </li>
 
-                    <li className="flex flex-row items-center justify-between pt-2 mt-2 border-t">
-                        <div className="flex flex-row items-center">
-                            <div className="w-6 h-6">
-                                <BankNotes />
-                            </div>
-                            <div className="ml-3 font-medium">Tunai</div>
-                        </div>
+                    <li className="group hover:bg-gray-400">
+                        <Link href={'/wallet'} className="flex flex-col px-3 pb-3">
+                            <div className="w-full border-t group-hover:border-gray-400"></div>
 
-                        <div className="font-medium">Rp2,500,000</div>
+                            <div className="flex flex-row items-center justify-between mt-3">
+                                <div className="flex flex-row items-center">
+                                    <div className="w-6 h-6">
+                                        <CreditCard />
+                                    </div>
+                                    <div className="ml-3 font-medium">Cash</div>
+                                </div>
+
+                                <div className="font-medium">Rp2,500,000</div>
+                            </div>
+                        </Link>
                     </li>
                 </ul>
             </section>
 
             {/* recent transactions */}
-            <section className="flex flex-col p-3 mt-5 bg-white rounded-lg">
-                <div className="flex flex-row items-center justify-between">
+            <section className="flex flex-col mt-5 bg-white rounded-lg">
+                <div className="flex flex-row items-center justify-between p-3">
                     <h2 className="text-lg font-semibold">Recent Transactions</h2>
                     <Link href={'/transactions'} className="font-medium text-green-400">
                         See all
@@ -69,28 +87,43 @@ export default function Home() {
                 </div>
 
                 <ul>
-                    <li className="flex flex-row items-center justify-between pt-2 mt-2 border-t">
-                        <div className="flex flex-col">
-                            <div className="font-medium">Transportation</div>
-                            <div className="text-sm text-gray-500">1 April 2023</div>
-                        </div>
-                        <div className="font-medium text-red-500">-Rp22,844</div>
+                    <li className="group hover:bg-gray-400">
+                        <Link href={'/wallet'} className="flex flex-col px-3 pb-2">
+                            <div className="w-full border-t group-hover:border-gray-400"></div>
+                            <div className="flex flex-row items-center justify-between mt-2">
+                                <div className="flex flex-col">
+                                    <div className="font-medium">Transportation</div>
+                                    <div className="text-sm text-gray-500 group-hover:text-white">1 April 2023</div>
+                                </div>
+                                <div className="font-medium text-red-500">-Rp22,844</div>
+                            </div>
+                        </Link>
                     </li>
 
-                    <li className="flex flex-row items-center justify-between pt-2 mt-2 border-t">
-                        <div className="flex flex-col">
-                            <div className="font-medium">Food & Beverages</div>
-                            <div className="text-sm text-gray-500">1 April 2023</div>
-                        </div>
-                        <div className="font-medium text-red-500">-Rp55,844</div>
+                    <li className="group hover:bg-gray-400">
+                        <Link href={'/wallet'} className="flex flex-col px-3 pb-2">
+                            <div className="w-full border-t group-hover:border-gray-400"></div>
+                            <div className="flex flex-row items-center justify-between mt-2">
+                                <div className="flex flex-col">
+                                    <div className="font-medium">Food & Beverages</div>
+                                    <div className="text-sm text-gray-500 group-hover:text-white">1 April 2023</div>
+                                </div>
+                                <div className="font-medium text-red-500">-Rp55,844</div>
+                            </div>
+                        </Link>
                     </li>
 
-                    <li className="flex flex-row items-center justify-between pt-2 mt-2 border-t">
-                        <div className="flex flex-col">
-                            <div className="font-medium">Salary</div>
-                            <div className="text-sm text-gray-500">28 March 2023</div>
-                        </div>
-                        <div className="font-medium text-blue-500">Rp6,787,844</div>
+                    <li className="group hover:bg-gray-400">
+                        <Link href={'/wallet'} className="flex flex-col px-3 pb-2">
+                            <div className="w-full border-t group-hover:border-gray-400"></div>
+                            <div className="flex flex-row items-center justify-between mt-2">
+                                <div className="flex flex-col">
+                                    <div className="font-medium">Salary</div>
+                                    <div className="text-sm text-gray-500 group-hover:text-white">28 March 2023</div>
+                                </div>
+                                <div className="font-medium text-blue-500">Rp6,787,844</div>
+                            </div>
+                        </Link>
                     </li>
                 </ul>
             </section>
