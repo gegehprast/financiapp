7486a1c36156b32294b7c0f18662ed6ac0a1bae8
gegehprast <gegeh.prast@gmail.com>
2023-04-03 04:42:46 +0700

    update layout

--- pages/account.tsx
+++ pages/account.tsx
@@ -5,58 +5,67 @@ import ReceiptPercent from '@/components/icons/ReceiptPercent'
 import RectangleGroup from '@/components/icons/RectangleGroup'
 import Tag from '@/components/icons/Tag'
 import Wallet from '@/components/icons/Wallet'
+import Link from 'next/link'
 import React from 'react'
 
 const Account = () => {
     return (
         <main>
-            <ul>
-                <li className="flex flex-row items-center justify-between p-2 pt-2 mt-2 border-t hover:bg-gray-500">
-                    <div className="flex flex-row items-center">
-                        <div className="w-6 h-6">
-                            <Wallet />
+            <ul className="mt-5 bg-white">
+                <li className="group hover:bg-gray-500">
+                    <Link href={'/wallets'} className="flex flex-row items-center justify-between p-4">
+                        <div className="flex flex-row items-center">
+                            <div className="w-6 h-6">
+                                <Wallet />
+                            </div>
+                            <div className="ml-3 font-medium">My Wallets</div>
                         </div>
-                        <div className="ml-3 font-medium">My Wallets</div>
-                    </div>
 
-                    <div className="w-6 h-6">
-                        <ChevronRight />
-                    </div>
+                        <div className="w-6 h-6">
+                            <ChevronRight />
+                        </div>
+                    </Link>
                 </li>
 
-                <li className="flex flex-row items-center justify-between p-2 pt-2 mt-2 border-t hover:bg-gray-500">
-                    <div className="flex flex-row items-center">
-                        <div className="w-6 h-6">
-                            <RectangleGroup />
+                <li className="group hover:bg-gray-500">
+                    <Link href={'/categories'} className="flex flex-row items-center justify-between p-4">
+                        <div className="flex flex-row items-center">
+                            <div className="w-6 h-6">
+                                <RectangleGroup />
+                            </div>
+                            <div className="ml-3 font-medium">Categories</div>
                         </div>
-                        <div className="ml-3 font-medium">Categories</div>
-                    </div>
 
-                    <div className="w-6 h-6">
-                        <ChevronRight />
-                    </div>
+                        <div className="w-6 h-6">
+                            <ChevronRight />
+                        </div>
+                    </Link>
                 </li>
 
-                <li className="flex flex-row items-center justify-between p-2 pt-2 mt-2 border-t hover:bg-gray-500">
-                    <div className="flex flex-row items-center">
-                        <div className="w-6 h-6">
-                            <ReceiptPercent />
+                <li className="group hover:bg-gray-500">
+                    <Link href={'/bills'} className="flex flex-row items-center justify-between p-4">
+                        <div className="flex flex-row items-center">
+                            <div className="w-6 h-6">
+                                <ReceiptPercent />
+                            </div>
+                            <div className="ml-3 font-medium">Bills</div>
                         </div>
-                        <div className="ml-3 font-medium">Bills</div>
-                    </div>
 
-                    <div className="w-6 h-6">
-                        <ChevronRight />
-                    </div>
+                        <div className="w-6 h-6">
+                            <ChevronRight />
+                        </div>
+                    </Link>
                 </li>
 
-                <li className="flex flex-row items-center justify-between p-2 pt-2 mt-2 border-t hover:bg-gray-500">
-                    <div className="flex flex-row items-center">
-                        <div className="w-6 h-6">
-                            <ArrowRightOnRectangle />
+                <li className="group hover:bg-gray-500">
+                    <Link href={'/logout'} className="flex flex-row items-center justify-between p-4">
+                        <div className="flex flex-row items-center">
+                            <div className="w-6 h-6">
+                                <ArrowRightOnRectangle />
+                            </div>
+                            <div className="ml-3 font-medium">Logout</div>
                         </div>
-                        <div className="ml-3 font-medium">Logout</div>
-                    </div>
+                    </Link>
                 </li>
             </ul>
         </main>
