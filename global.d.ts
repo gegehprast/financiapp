1d8afd695b1c94c0e859921a6d5448ccf1147b61
gegehprast <gegeh.prast@gmail.com>
2023-04-02 20:16:50 +0700

    global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { IUserDoc } from './models/User'
+
+export type MessageResponse = { message?: string }
+
+export type ApiHandler<ResponseData = any> = (req: NextApiRequest, res: NextApiResponse<MessageResponse | ResponseData>) => Promise<void>
+
+export type ApiMiddleware = (handler: ApiHandler) => ApiHandler
+
+export interface Auth {
+    isLoggedIn: boolean
+    user: Partial<Omit<IUserDoc, 'password'>> | null
+}
+
+declare module 'iron-session' {
+    interface IronSessionData {
+        auth?: Auth
+    }
+}
