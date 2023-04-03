1812f42acdc9c352b5c7cb0f9cb7ded92c6abd74
gegehprast <gegeh.prast@gmail.com>
2023-04-04 06:01:52 +0700

    cleaning and prevent flashing

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -1,38 +1,21 @@
-import Link from 'next/link'
 import React, { ReactElement, useRef } from 'react'
-import ArrowRightOnRectangle from './icons/ArrowRightOnRectangle'
 import Menu from './Menu'
 import useAuth from '@/hooks/useAuth'
-import { useRouter } from 'next/router'
-import axios from 'axios'
 
 interface Props {
     children: ReactElement
 }
 
 const MainLayout = ({ children }: Props) => {
-    const router = useRouter()
     const { auth } = useAuth({ redirectTo: '/login' })
     const content = useRef<HTMLDivElement>(null)
-
-    const handleLogout = async () => {
-        try {
-            const resp = await axios.post('/api/logout')
-
-            if (resp.status === 200) {
-                router.push('/login')
-            }
-        } catch {
-            router.push('/login')
-        }
-    }
 
     return (
         <div className="relative w-full h-screen mx-auto overflow-hidden bg-gray-200 lg:w-1/2 xl:w-1/4">
             {/* content */}
             <div className="relative w-full h-screen pb-[3rem] overflow-hidden">
                 <div className="w-full h-full overflow-y-auto" ref={content}>
-                    {children}
+                    {auth.isLoggedIn ? children : null}
                 </div>
             </div>
 
