5f4fa33bed1884b8f11055b101624dc1ccd08ec6
gegehprast <gegeh.prast@gmail.com>
2023-04-04 07:39:03 +0700

    icon string

--- models/Category.ts
+++ models/Category.ts
@@ -1,4 +1,3 @@
-import { IconMap } from '@/components/Icon'
 import mongoose, { Schema, Document } from 'mongoose'
 
 export interface ICategoryDoc extends Document {
@@ -6,7 +5,7 @@ export interface ICategoryDoc extends Document {
     categoryId?: mongoose.Types.ObjectId
     name: string
     type: 'expense' | 'income' | 'debt'
-    icon: keyof typeof IconMap
+    icon: string
     createdAt: Date
     updatedAt: Date
 }
@@ -28,7 +27,7 @@ export const CategorySchema: Schema = new Schema(
             required: true,
         },
         type: {
-            type: Number,
+            type: String,
             required: false,
             default: 'expense',
             enum: ['expense', 'income', 'debt'],
@@ -36,13 +35,12 @@ export const CategorySchema: Schema = new Schema(
         icon: {
             type: String,
             required: true,
-            enum: Object.keys(IconMap),
         },
     },
     { timestamps: true }
 )
 
-CategorySchema.index({ userId: 1, name: 1 }, { unique: true })
+CategorySchema.index({ userId: 1, name: 1, type: 1 }, { unique: true })
 
 const Category: mongoose.Model<ICategoryDoc, {}, {}, {}, any> = mongoose.models.Category || mongoose.model<ICategoryDoc>('Category', CategorySchema)
 
