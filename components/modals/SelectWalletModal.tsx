b8a49dd08dd988593d374923e8e5fd3481ce9f38
gegehprast <gegeh.prast@gmail.com>
2023-04-05 08:04:09 +0700

    SelectWalletModal

---
+++ components/modals/SelectWalletModal.tsx
@@ -0,0 +1,54 @@
+import { useModal } from '@/contexts/ModalContext'
+import useWallet from '@/hooks/useWallet'
+import { IWalletDoc } from '@/models/Wallet'
+import React from 'react'
+import { IoArrowBackOutline, IoCardOutline, IoCashOutline, IoCloseOutline } from 'react-icons/io5'
+
+interface SelectWalletModalProps {
+    show: boolean
+    select: (wallet: IWalletDoc) => void
+}
+
+const SelectWalletModal: React.FC<SelectWalletModalProps> = ({ show, select }) => {
+    const { walletModal } = useModal()
+    const { wallets, isSuccess } = useWallet()
+
+    return (
+        <div
+            className={`fixed w-full left-0 top-0 bg-gray-200 h-[calc(100vh-4rem)] transition-transform duration-300 ease-in-out ${
+                show ? 'translate-y-0' : 'translate-y-full'
+            }`}
+        >
+            <header className="flex flex-row items-center p-4 bg-white">
+                <button type="button" onClick={walletModal.close}>
+                    <IoArrowBackOutline className="w-6 h-6" />
+                </button>
+
+                <h1 className="ml-6 text-lg font-semibold">My Wallets</h1>
+            </header>
+
+            <ul className="mt-5 bg-white">
+                {wallets.map((wallet) => (
+                    <li className="group hover:bg-gray-400" key={wallet._id}>
+                        <button onClick={() => {select(wallet); walletModal.close()}} type="button" className="flex flex-row items-center w-full p-4">
+                            {wallet.type === 'cash' ? (
+                                <IoCashOutline className="w-8 h-8 group-hover:text-white" />
+                            ) : (
+                                <IoCardOutline className="w-8 h-8 group-hover:text-white" />
+                            )}
+
+                            <div className="flex flex-col items-start justify-center ml-4">
+                                <div className="font-medium group-hover:text-white">{wallet.name}</div>
+                                <div className="text-gray-500 group-hover:text-white">
+                                    {new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(wallet.balance)}
+                                </div>
+                            </div>
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default SelectWalletModal
