feab95363336ac7827387451505ef2dc7375b868
gegehprast <gegeh.prast@gmail.com>
2023-04-06 05:30:18 +0700

    user

--- pages/api/category/index.ts
+++ pages/api/category/index.ts
@@ -1,16 +1,16 @@
 import withCORS from '@/middlewares/withCORS'
 import withDB from '@/middlewares/withDB'
 import { NextApiRequest, NextApiResponse } from 'next'
-import Category, { ICategoryDoc } from '@/models/Category'
 import { MessageResponse } from '@/global'
 import { withIronSessionApiRoute } from 'iron-session/next'
 import { sessionOptions } from '@/lib/session'
 import methodGet from '@/middlewares/methodGet'
 import authenticated from '@/middlewares/authenticated'
+import Category, { ICategoryDoc } from '@/models/Category'
 
 async function index(req: NextApiRequest, res: NextApiResponse<MessageResponse | ICategoryDoc[]>) {
     try {
-        const categories = await Category.find({ $or: [{ userId: req.session.auth.user._id }, { userId: null }] }).sort({ createdAt: 1 })
+        const categories = await Category.find({ $or: [{ user: req.session.auth.user._id }, { user: null }] }).sort({ createdAt: 1 })
 
         return res.json(categories)
     } catch (error) {
