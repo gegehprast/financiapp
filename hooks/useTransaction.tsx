7edf7cc58b3cf9bba04441d387cfc7645aed2697
gegehprast <gegeh.prast@gmail.com>
2023-04-07 06:48:58 +0700

    mutation

--- hooks/useTransaction.tsx
+++ hooks/useTransaction.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useQuery } from '@tanstack/react-query'
+import { MutationFunction, useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
 import { IPopulatedTransactionDoc, ITransactionDoc } from '@/models/Transaction'
 
@@ -9,14 +9,28 @@ export interface IGetTransactionsQuery {
     endDate?: string
 }
 
+export interface IUseTransactionProps {
+    mutationFn?: MutationFunction
+    query: IGetTransactionsQuery
+}
+
 const getTransactions = (query: IGetTransactionsQuery): Promise<IPopulatedTransactionDoc[]> =>
     axios
         .get(`/api/transaction?startDate=${query.startDate || ''}&endDate=${query.endDate || ''}&limit=${query.limit || ''}`)
         .then((res) => res.data)
 
-export default function useTransaction(query: IGetTransactionsQuery) {
+export default function useTransaction({ mutationFn, query }: IUseTransactionProps = { query: {} }) {
+    const queryClient = useQueryClient()
     const { data, isLoading, isSuccess } = useQuery({ queryKey: ['transactions', query], queryFn: () => getTransactions(query), initialData: [] })
     const [transactions, setTransactions] = React.useState<IPopulatedTransactionDoc[]>(data)
+
+    const mutation = useMutation({
+        mutationFn: mutationFn,
+        onSuccess: () => {
+            // Invalidate and refetch
+            queryClient.invalidateQueries({ queryKey: ['wallets'] })
+        },
+    })
 
     React.useEffect(() => {
         const trans = data.map((t) => {
@@ -31,5 +45,5 @@ export default function useTransaction(query: IGetTransactionsQuery) {
         setTransactions(trans)
     }, [data])
 
-    return { transactions, isLoading, isSuccess }
+    return { transactions, isLoading, isSuccess, mutation }
 }
--- hooks/useWallet.tsx
+++ hooks/useWallet.tsx
@@ -1,11 +1,29 @@
-import { useQuery } from '@tanstack/react-query'
+import { MutationFunction, useMutation, useQuery, useQueryClient, Mutation } from '@tanstack/react-query'
 import axios from 'axios'
 import { IWalletDoc } from '@/models/Wallet'
 
-const getWallets = (limit?: number): Promise<IWalletDoc[]> => axios.get(`/api/wallet?limit=${limit}`).then((res) => res.data)
+export interface IGetWalletsQuery {
+    limit?: number
+}
 
-export default function useWallet(limit?: number) {
-    const { data: wallets, isLoading, isSuccess } = useQuery({ queryKey: ['wallets', limit], queryFn: () => getWallets(limit), initialData: [] })
+export interface IUseWalletProps {
+    mutationFn?: MutationFunction
+    query: IGetWalletsQuery
+}
 
-    return { wallets, isLoading, isSuccess }
+const getWallets = (query: IGetWalletsQuery): Promise<IWalletDoc[]> => axios.get(`/api/wallet?limit=${query.limit || ''}`).then((res) => res.data)
+
+export default function useWallet({ mutationFn, query }: IUseWalletProps = { query: {} }) {
+    const queryClient = useQueryClient()
+    const { data: wallets, isLoading, isSuccess } = useQuery({ queryKey: ['wallets', query], queryFn: () => getWallets(query), initialData: [] })
+
+    const mutation = useMutation({
+        mutationFn: mutationFn,
+        onSuccess: () => {
+            // Invalidate and refetch
+            queryClient.invalidateQueries({ queryKey: ['wallets'] })
+        },
+    })
+
+    return { wallets, isLoading, isSuccess, mutation }
 }
