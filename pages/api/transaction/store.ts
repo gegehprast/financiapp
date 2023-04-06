9d221ac3a98ea70cf2402dea6a1e5a27eee1aaa2
gegehprast <gegeh.prast@gmail.com>
2023-04-07 06:53:16 +0700

    update wallet balance

--- pages/api/transaction/store.ts
+++ pages/api/transaction/store.ts
@@ -9,20 +9,36 @@ import { withIronSessionApiRoute } from 'iron-session/next'
 import Transaction, { ITransactionDoc } from '@/models/Transaction'
 import mongoose from 'mongoose'
 import Wallet from '@/models/Wallet'
+import Category from '@/models/Category'
 
 async function store(req: NextApiRequest, res: NextApiResponse<MessageResponse | ITransactionDoc>) {
     try {
-        const wallet = await Wallet.find({ user: req.session.auth.user._id, _id: req.body.walletId })
+        const wallet = await Wallet.findOne({ user: req.session.auth.user._id, _id: req.body.walletId })
+        const category = await Category.findOne({ _id: req.body.categoryId })
+
+        if (!wallet) {
+            return res.status(404).json({ message: 'Wallet not found.' })
+        }
+
+        if (!category) {
+            return res.status(404).json({ message: 'Category not found.' })
+        }
+
+        // save transaction
         const transaction = new Transaction({
             user: req.session.auth.user._id,
             category: new mongoose.Types.ObjectId(req.body.categoryId),
             wallet: new mongoose.Types.ObjectId(req.body.walletId),
-            amount: req.body.amount,
+            amount: parseFloat(req.body.amount),
             notes: req.body.notes,
             date: new Date(req.body.date),
         })
 
         await transaction.save()
+        
+        // save wallet
+        wallet.balance += category.type === 'income' ? transaction.amount : -transaction.amount
+        await wallet.save()
 
         return res.json(transaction)
     } catch (error) {
