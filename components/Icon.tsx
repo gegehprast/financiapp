c38d48ea1fcdef56e8962a87b4335fcebd22421f
gegehprast <gegeh.prast@gmail.com>
2023-04-09 11:20:15 +0700

    no lazy

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,5 +1,70 @@
 import React from 'react'
-import { IoRadioButtonOffOutline } from 'react-icons/io5'
+import {
+    IoRadioButtonOffOutline,
+    IoCarSportOutline,
+    IoFastFoodOutline,
+    IoBagOutline,
+    IoFlashOutline,
+    IoWaterOutline,
+    IoWifiOutline,
+    IoCellularOutline,
+    IoTicketOutline,
+    IoShieldCheckmarkOutline,
+    IoMedkitOutline,
+    IoConstructOutline,
+    IoSchoolOutline,
+    IoBookOutline,
+    IoFilmOutline,
+    IoGameControllerOutline,
+    IoAirplaneOutline,
+    IoPhonePortraitOutline,
+    IoShirtOutline,
+    IoHeartOutline,
+    IoCloudOutline,
+    IoDiamondOutline,
+    IoWomanOutline,
+    IoPeopleOutline,
+    IoReceiptOutline,
+    IoReturnDownForwardOutline,
+    IoAppsOutline,
+    IoWalletOutline,
+    IoCashOutline,
+    IoGiftOutline,
+    IoReturnDownBackOutline,
+} from 'react-icons/io5'
+
+const IconMap = {
+    IoCarSportOutline: IoCarSportOutline,
+    IoFastFoodOutline: IoFastFoodOutline,
+    IoBagOutline: IoBagOutline,
+    IoFlashOutline: IoFlashOutline,
+    IoWaterOutline: IoWaterOutline,
+    IoWifiOutline: IoWifiOutline,
+    IoCellularOutline: IoCellularOutline,
+    IoTicketOutline: IoTicketOutline,
+    IoShieldCheckmarkOutline: IoShieldCheckmarkOutline,
+    IoMedkitOutline: IoMedkitOutline,
+    IoConstructOutline: IoConstructOutline,
+    IoSchoolOutline: IoSchoolOutline,
+    IoBookOutline: IoBookOutline,
+    IoFilmOutline: IoFilmOutline,
+    IoGameControllerOutline: IoGameControllerOutline,
+    IoAirplaneOutline: IoAirplaneOutline,
+    IoPhonePortraitOutline: IoPhonePortraitOutline,
+    IoShirtOutline: IoShirtOutline,
+    IoHeartOutline: IoHeartOutline,
+    IoCloudOutline: IoCloudOutline,
+    IoDiamondOutline: IoDiamondOutline,
+    IoWomanOutline: IoWomanOutline,
+    IoPeopleOutline: IoPeopleOutline,
+    IoReceiptOutline: IoReceiptOutline,
+    IoReturnDownForwardOutline: IoReturnDownForwardOutline,
+    IoAppsOutline: IoAppsOutline,
+    IoWalletOutline: IoWalletOutline,
+    IoCashOutline: IoCashOutline,
+    IoGiftOutline: IoGiftOutline,
+    IoReturnDownBackOutline: IoReturnDownBackOutline,
+}
 
 interface IconProps {
     icon: string
@@ -7,13 +72,9 @@ interface IconProps {
 }
 
 const Icon: React.FC<IconProps> = ({ icon, className }) => {
-    const IconComponent = React.lazy(() => import('react-icons/io5').then((module) => ({ default: module[icon] as any })))
+    const Component = IconMap[icon as keyof typeof IconMap] ? IconMap[icon as keyof typeof IconMap] : IoRadioButtonOffOutline
 
-    return (
-        <React.Suspense fallback={<IoRadioButtonOffOutline />}>
-            <IconComponent className={className} />
-        </React.Suspense>
-    )
+    return <React.Suspense fallback={<IoRadioButtonOffOutline />}>{<Component className={className} />}</React.Suspense>
 }
 
 export default Icon
