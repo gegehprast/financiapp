818bf6aeaad37530059a74479b8cd7113e3318a6
gegehprast <gegeh.prast@gmail.com>
2023-04-14 01:42:28 +0700

    lang, icons

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,12 +1,9 @@
-import ArrowRightOnRectangle from '@/components/icons/ArrowRightOnRectangle'
 import ChevronRight from '@/components/icons/ChevronRight'
-import ReceiptPercent from '@/components/icons/ReceiptPercent'
-import RectangleGroup from '@/components/icons/RectangleGroup'
-import Wallet from '@/components/icons/Wallet'
 import axios from 'axios'
 import Link from 'next/link'
 import Router from 'next/router'
 import React from 'react'
+import { IoAppsOutline, IoLogOutOutline, IoSettingsOutline, IoWalletOutline } from 'react-icons/io5'
 
 const Account = () => {
     const handleLogout = () => {
@@ -21,9 +18,7 @@ const Account = () => {
                 <li className="group hover:bg-gray-500">
                     <Link href={'/wallets'} className="flex flex-row items-center justify-between p-4">
                         <div className="flex flex-row items-center">
-                            <div className="w-6 h-6">
-                                <Wallet />
-                            </div>
+                            <IoWalletOutline className="w-6 h-6" />
                             <div className="ml-3 font-medium">My Wallets</div>
                         </div>
 
@@ -36,10 +31,21 @@ const Account = () => {
                 <li className="group hover:bg-gray-500">
                     <Link href={'/categories'} className="flex flex-row items-center justify-between p-4">
                         <div className="flex flex-row items-center">
-                            <div className="w-6 h-6">
-                                <RectangleGroup />
-                            </div>
+                            <IoAppsOutline className="w-6 h-6" />
                             <div className="ml-3 font-medium">Kategori</div>
+                        </div>
+
+                        <div className="w-6 h-6">
+                            <ChevronRight />
+                        </div>
+                    </Link>
+                </li>
+
+                <li className="group hover:bg-gray-500">
+                    <Link href={'/setting'} className="flex flex-row items-center justify-between p-4">
+                        <div className="flex flex-row items-center">
+                            <IoSettingsOutline className="w-6 h-6" />
+                            <div className="ml-3 font-medium">Pengaturan</div>
                         </div>
 
                         <div className="w-6 h-6">
@@ -51,9 +57,7 @@ const Account = () => {
                 <li className="cursor-pointer group hover:bg-gray-500" onClick={handleLogout}>
                     <button type="button" className="flex flex-row items-center justify-between p-4">
                         <div className="flex flex-row items-center">
-                            <div className="w-6 h-6">
-                                <ArrowRightOnRectangle />
-                            </div>
+                            <IoLogOutOutline className="w-6 h-6" />
                             <div className="ml-3 font-medium">Keluar</div>
                         </div>
                     </button>
